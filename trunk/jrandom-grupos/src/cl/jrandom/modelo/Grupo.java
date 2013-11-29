faa0576c34acc1553b125f0c65ba81e8637e6865
patodeath@gmail.com <patodeath@gmail.com@304a61d3-327e-9e35-4f86-1af9cac92424>
2013-11-29 18:19:02 +0000

    Primera versión

---
+++ trunk/jrandom-grupos/src/cl/jrandom/modelo/Grupo.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cl.jrandom.modelo;
+
+/**
+ *
+ * @author profesor
+ */
+public class Grupo {
+    private int numero;
+    private String nombre;
+
+    public Grupo(int numero, String nombre) {
+        this.numero = numero;
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    /*Esto es lo que se visualizará en esa lista.
+     el numero y el nombre de la siguiente forma:
+
+     1) Grupo 1*/
+    @Override
+    public String toString(){
+        return this.numero + ") " +nombre;
+    }
+}
